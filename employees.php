5c4ae39f99f85a9b01194e299298aad9321fcead
cclp94 <cclp94@gmail.com>
2017-03-30 17:27:24 -0400

    fixed user require

--- employees.php
+++ employees.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once './app/models/employee.php';
+    require_once './app/models/user.php';
     if(!isset($_SESSION) || !isset($_SESSION['user'])){
         session_unset();
         session_destroy();
