37a4c954dcb0a112c3148d721065b5e0684a9a84
cclp94 <cclp94@gmail.com>
2017-03-30 16:28:20 -0400

    Create php models

---
+++ app/models/project.php
@@ -0,0 +1,29 @@
+<?php
+require_once './supervisor.php';
+require_once './employee.php';
+    class Project{
+        var $projectID, $name, $supervisor, $startDate, $endDate,$deadline, $budget, $client;
+
+        function Project($id, $name, $supervisor, $startDate, $endDate, $deadline, $budget, $client){
+            $this->id = $id;
+            $this->name = $name;
+            $this->supervisor = $supervisor;
+            $this->startDate = $startDate;
+            $this->endDate = $endDate;
+            $this->deadline = $deadline;
+            $this->budget = $budget;
+            $this->client = $client;
+        }
+
+        public static function getProject($id){
+            // Get project from db
+        }
+
+        public static function getProjectMock($id){
+            // Get project from db
+            return new Project($id, "Mock Project", )
+        }
+    }
+
+
+?>
