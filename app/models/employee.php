c667d6cb067d02a451734697ba940f83ec87124f
Wishe Deom <wishedeom@gmail.com>
2017-04-02 18:49:08 -0400

    path

--- app/models/employee.php
+++ app/models/employee.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __ROOT__.'/app/connection.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/connection.php';
 
     class Employee{
         var $SIN, $name, $title, $wage;
