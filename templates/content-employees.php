b7f12986ef7c826c72a5b94b93ecc5663a9e116f
Wishe Deom <wishedeom@gmail.com>
2017-04-03 22:39:37 -0400

    test

--- templates/content-employees.php
+++ templates/content-employees.php
@@ -52,7 +52,7 @@
                 <input class="form-control" type="number" name="wage" value="<?php echo number_format($employee->wage, 2) ?>" required/></br>
             </div>
     </div>
-    <div class = "btn-group center-block">
+    <div class = "row center-block">
         <input class="btn btn-primary btn-lg" type="submit" name="update" value="Update Employee"/>
         <input class="btn btn-primary btn-lg" type="submit" name="delete" value="Delete Employee"/>
     </div>
