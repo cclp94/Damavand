af885744538b6de8149af1828e260388b52c7fbc
Michael Deom <wishedeom@gmail.com>
2017-04-08 15:33:46 -0400

    Time ratio

--- app/models/task.php
+++ app/models/task.php
@@ -118,5 +118,22 @@
             //return $conn->query($sql)->fetch_assoc();
             return (int) $conn->query($sql);
         }
+
+        static function getAllComplete() {
+            $conn = connect();
+            $sql = "Select * from Task order by phase where endDate is not null;";
+            $result = $conn->query($sql);
+            $tasks = [];
+            while ($result && $row = $result->fetch_assoc()) {
+                $tasks[] = Task::fromRow($row);
+            }
+            return $tasks;
+        }
+
+        static function estimatedTimeOfComplete() {
+            $conn = connect();
+            $sql = "Select sum(estimatedTime) from Task having endDate is not null;";
+            return (int) $conn->query($sql);
+        }
     }
 ?>
--- app/report.php
+++ app/report.php
@@ -2,23 +2,59 @@
 require_once 'connection.php';
 
 date_default_timezone_set("America/Montreal");
-//$startDate = new DateTime($project->startDate);
 $startDate = DateTime::createFromFormat('Y-m-d', $project->startDate);
-var_dump($startDate);
-//$currentDate = new DateTime(date("y-m-d"));
 $currentDate = DateTime::createFromFormat('Y-m-d', date("Y-m-d"));
-var_dump($currentDate);
-$difference = $currentDate - $startDate;
+$estimatedTime = Task::estimatedTimeOfComplete();
+$actualTime = $currentDate->diff($startDate)->days;
+$timeRatio = $estimatedTime / $actualTime;
+
 echo $startDate->format("y-m-d") . "<br>";
 echo $currentDate->format("y-m-d") . "<br>";
-echo $difference . "<br>"   ;
+echo $actualTime . "<br>";
+echo $estimatedTime . "<br>";
+
 $completeTaskCount = Task::completeCount();
+$completeTasks = Task::getAllComplete();
 
 $taskCount = count($tasks);
 echo $project->name . "<br>";
 echo "Tasks Complete: $completeTaskCount / $taskCount <br>";
+echo "Project Duration (Estimated / Actual): $estimatedTime / $actualTime (" . percentString($timeRatio) . ") <br>";
+echo "    " . progressColourString($timeRatio) . "<br>";
 ?>
 
 <?php
+
+function progressString($ratio) {
+    if ($ratio <= 0.0)
+        return "No progress";
+    if ($ratio <= 0.5)
+        return "Way behind schedule";
+    if ($ratio <= 0.9)
+        return "Behind schedule";
+    if ($ratio <= 1.1)
+        return "On schedule";
+    if ($ratio <= 1.5)
+        return "Ahead of schedule";
+    return "Way ahead of schedule";
+}
+
+function progressColour($ratio) {
+    if ($ratio < 0.9)
+        return "#ff0000";
+    if ($ratio < 1.1)
+        return "#0000cc";
+    return "#00cc00";
+}
+
+function progressColourString($ratio) {
+    $msg = progressString($ratio);
+    $colour = progressColour($ratio);
+    return "<div style=\"color:$colour;\">$msg</div>";
+}
+
+function percentString($x) {
+    return round($x * 100).'%';
+}
 
 ?>
